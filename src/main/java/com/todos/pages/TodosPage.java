b8566f951324fdc23c1b095bfcbbb4df61805982
unknown <halimasouei2014@gmail.com>
2024-07-23 00:00:31 +0200

    updated project

--- src/main/java/com/todos/pages/TodosPage.java
+++ src/main/java/com/todos/pages/TodosPage.java
@@ -1,5 +1,54 @@
 package com.todos.pages;
 
-public class TodosPage {
+import java.io.IOException;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.todos.utils.BasePage;
+
+public class TodosPage extends BasePage {
+
+	public TodosPage() throws IOException {
+		 PageFactory.initElements(driver, this);
+		 
+	}
+	@FindBy(how = How.XPATH, using = "//input[@placeholder='What needs to be done?']")
+	public static WebElement inputField;
+	
+	@FindBy(how = How.XPATH, using = "//label[normalize-space()='Adopter les bonnes pratiques de test']")
+	public static WebElement todoText;
+	
+	@FindBy(how = How.XPATH, using = "//input[@type='checkbox']")
+	public static WebElement checkbox;
+	
+	public void fillTodo(String todo) {
+		inputField.sendKeys(todo);
+		inputField.sendKeys(Keys.ENTER);
+		
+	}
+	
+	public Boolean isElementDisplayed(WebElement element) {
+		return element.isDisplayed();
+	}
+	
+	public String checkElementContain(WebElement element) {
+		return element.getText();
+		
+	}
+	
+	public Boolean isElementSelected(WebElement element) {
+		return element.isSelected();
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
--- src/test/java/com/todods/tests/AddTodoTest.java
+++ src/test/java/com/todods/tests/AddTodoTest.java
@@ -1,5 +1,37 @@
 package com.todods.tests;
 
-public class AddTodoTest {
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import com.todos.pages.TodosPage;
+import com.todos.utils.Setup;
+
+public class AddTodoTest extends Setup {
+
+	public AddTodoTest() throws IOException {
+		super();
+
+	}
+
+	TodosPage todoPage;
+
+	@Test
+	public void iCanAddTodo() throws IOException, InterruptedException {
+		todoPage = new TodosPage();
+		boolean checkFieldTodo = todoPage.isElementDisplayed(TodosPage.inputField);
+		Assert.assertTrue(checkFieldTodo);
+		todoPage.fillTodo(prop.getProperty("todo1"));
+
+		Thread.sleep(3000);
+		String textTodo = todoPage.checkElementContain(TodosPage.todoText);
+		Assert.assertEquals(textTodo, prop.getProperty("todo1"));
+
+		Thread.sleep(3000);
+		boolean checkbox = todoPage.isElementSelected(TodosPage.checkbox);
+		Assert.assertFalse(checkbox);
+
+	}
 
 }
